a470b5c7f90f3ab7ebc5c10872c08e1ea8efdc2d
yani82 <yani.iman1@gmail.com>
2022-03-15 01:14:03 -0400

    create and build Product.jsx

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Grid from '@material-ui/core';
 
-const Products() {
-  <main></main>  
+const products = [
+    {id: 1, name: 'T-shirt', description: 'All Smiles in NY'},
+    {id: 2, name: 'Hoodie', description: '2002 Lets Lepak zip-up' }
+];
+
+const Products = () => {
+  <main>
+      <Grid container justify="center" spacing={4}>
+            {products.map((products) - (
+                <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product />
+                </Grid>
+            ))}
+      </Grid>
+  </main>  
 }
 
 export default Products; 
