290fa640420b6c54523c0532ba2039463fc034a8
yani82 <yani.iman1@gmail.com>
2022-03-16 00:22:56 -0400

    stubbed out Product, synced w/ Products and created styles.js

---
+++ src/components/products/Product.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
+import useStyles from './styles'; 
+
+const Product = ({ product }) => {
+    const classes = useStyles(); 
+    
+  return (
+    <Card classname={classes.root}>
+        <CardMedia className={classes.media} image='' title={product.name} />
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography variant='h5' gutterBottom> 
+                    {product.name} 
+                </Typography>
+                <Typography variant='h5'>
+                    {product.price} 
+                </Typography>
+            </div>
+            <Typography variant='h2' color='textSecondary'> 
+                {product.description}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.CardActions}>
+            <IconButton aria-label='Add to Cart'> 
+                <AddShoppingCart />
+            </IconButton>
+        </CardActions>
+    </Card>
+  );
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product)
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products = [
-    {id: 1, name: 'T-shirt', description: 'All Smiles in NY'},
-    {id: 2, name: 'Hoodie', description: '2002 Lets Lepak zip-up' }
+    {id: 1, name: 'T-shirt', description: 'All Smiles in NY', price: '$45'},
+    {id: 2, name: 'Hoodie', description: '2002 Lets Lepak zip-up', price: '$55'}
 ];
 
 const Products = () => {
@@ -11,7 +11,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
             {products.map((products) - (
                 <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product />
+                    <Product product={product}/>
                 </Grid>
             ))}
       </Grid>
