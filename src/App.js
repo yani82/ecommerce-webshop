42b2e5f7280cedf4bebffd22f5e38d422966e60b
yani82 <yani.iman1@gmail.com>
2022-05-11 00:07:56 -0400

    made cart dynamic by adding totalItems

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
   return (
     <div>
-      <Navbar />
+      <Navbar totalItems = {cart.total_items} />
       <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import logo from '../../assets/commerce.png'
 import useStyles from './styles'; 
 
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles()
   return (
     <>
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <div className={classes.grow}/>
                 <div className={classes.button}>
                     <IconButton aria-label="Show cart items" color='inherit'> 
-                        <Badge badgeContent={2} color='secondary'>
+                        <Badge badgeContent={totalItems} color='secondary'>
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
