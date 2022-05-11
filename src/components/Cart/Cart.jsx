c577c31dd2416b88be64081d7ecc9847b071a4a5
yani82 <yani.iman1@gmail.com>
2022-05-11 00:16:14 -0400

    build out Cart, to be able to display what's in it

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'; 
 
 const Cart = () => {
+
+  const isEmpty = true; 
+
+  const EmptyCart = () => {
+      <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+  }
+    
   return (
-    <div>Cart</div>
+    <Container>
+        <div className={classes.toolbar} /> 
+        <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+        { isEmpty ? <EmptyCart /> : <FilledCart /> }
+    </Container>
   )
 }
 
