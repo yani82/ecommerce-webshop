12310f605163f333967c4d3b68cf0c5262f3c738
yani82 <yani.iman1@gmail.com>
2022-07-15 02:44:29 -0400

    building out cartitem

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
-import useStyles from './styles'; 
+import useStyles from './cartItemStyles'; 
 
 const CartItem = () => {
+const classes = useStyles();  
+
   return (
-    <div>
-        
-    </div>
+    <Card>
+        <CardMedia image={item.media.source} />
+    </Card>
   )
 }
 
