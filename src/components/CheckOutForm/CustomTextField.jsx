12035a08925442def968755a394e8a02ccd53fec
yani82 <yani.iman1@gmail.com>
2022-12-28 04:56:58 -0500

    refacoring checkout process to ibclude CustomTextField

--- src/components/CheckOutForm/CustomTextField.jsx
+++ src/components/CheckOutForm/CustomTextField.jsx
@@ -8,8 +8,8 @@ function FormInput({ name, label }) {
   
     return (
     <Grid item xs = {12} sm = {6}>
-        <Controller as = {TextField} control = {control} fullWidth name = {name} label = {label} required /> 
-        {/* error = {isError} */}
+        <Controller as = {TextField} defaultValue="" control = {control} fullWidth name = {name} label = {label} required /> 
+        error = {isError}
     </Grid>
   );
 }
